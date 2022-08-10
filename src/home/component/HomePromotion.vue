c008a2dd0e65c46f15ff612ecf2c6bbb6387d68f
Huynh Quoc Phong <phonghq@dtsmart.vn>
2022-08-11 03:51:18 +0700

    update

--- src/home/component/HomePromotion.vue
+++ src/home/component/HomePromotion.vue
@@ -1,9 +1,10 @@
 <template>
                 <div class="mb-5 px-5 d-flex flex-column align-items-center">
+                <img class="banner-img" src="../../../public/images/banner.png" alt="">
                 <img class="logo-white mb-3" src="../../../public/images/logo-white.png" />
                 <p
                     class="fs-2 text-white fw-bold text-center"
-                >GIÁNG SINH AN LÀNH - SĂN SALE CÙNG NEWZENCLOUD</p>
+                >GIÁNG SINH AN LÀNH -- SĂN SALE CÙNG NEWZENCLOUD</p>
                 <el-button class="fs-6 my-3 fw-bold" color="#0DD4D4">Đăng kí ngay</el-button>
                 <p class="my-4 fs-6 text-white text-center">
                     Hosting - Cloud VPS - SLL Bảo mật - Email - Phần mềm
